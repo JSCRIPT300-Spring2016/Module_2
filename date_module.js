19947a6095ad660864322d3faf0f814930e8882a
lperry <laurellperry@gmail.com>
2016-04-21 09:49:10 -0700

    linted date module

--- date_module.js
+++ date_module.js
@@ -2,52 +2,47 @@
 // set a variable for the date using built in js date function
 // setDate is a method that allows the value of the date variable to be changed by user input
 // user enters a new date which is saved to variable
-var myDate = new Date();
-var foo = (function iife() {
+
+var foo = ( function iife() {
+  var myDate = new Date();
 
   function setDate() {
     var userInputDate;
-    if (typeof userInputDate === 'undefined') {
-      return myDate;
-    }
-    else {
+    if (typeof userInputDate !== 'undefined') {
       myDate = userInputDate;
-
-      return myDate;
-    };
-       }
-    
-
-    function getDate() {
-        myDate.getDate({ format: "milliseconds" });
-        myDate.getDate({ format: "formatted" });
     }
-    
-      function getDayName() {
-        console.log(myDate.getDayName()); // "Monday"
-    }
+  }
 
-      function getMonthName() {
-        console.log(myDate.getMonthName()); // "April"
-    }
+  function getDate() {
+    myDate.getDate({ format: 'milliseconds' });
+    myDate.getDate({ format: 'formatted' });
+  }
 
-      function isFuture() {
-        console.log(myDate.isFuture()); // false
-    }
+  function getDayName() {
+    console.log(myDate.getDayName()); // "Monday"
+  }
 
-      function isToday() {
-        console.log(myDate.isToday()); // true
-    }
+  function getMonthName() {
+    console.log(myDate.getMonthName()); // "April"
+  }
+
+  function isFuture() {
+    console.log(myDate.isFuture()); // false
+  }
+
+  function isToday() {
+    console.log(myDate.isToday()); // true
+  }
 
 
-    return {
-        setDate: myDate,
-        getDate: myDate,
-        getDayName: getDayName,
-        getMonthName: getMonthName,
-        isFuture: isFuture,
-        isToday: isToday
-    };
+  return {
+    setDate: setDate,
+    getDate: getDate,
+    getDayName: getDayName,
+    getMonthName: getMonthName,
+    isFuture: isFuture,
+    isToday: isToday
+  };
 })();
 
-foo.bar(); // 'baz'
+foo.isFuture(); // 'baz'
