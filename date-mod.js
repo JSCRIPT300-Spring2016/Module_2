a9a73b545762025d762df8a23c84bea3e77c6553
sean c <sean@scavanaugh.com>
2016-04-21 13:58:10 -0500

    First commit

---
+++ date-mod.js
@@ -0,0 +1,69 @@
+var myObj = { function(){
+
+		//sets the day of the month to the date object
+		function setDate(){
+			var sd = new Date();
+			sd.setDate(15);
+		};
+
+		//returns the day of the month (from 1 to 31) for the specified date.
+		function getDate(){
+			var d = new Date();
+			d.getDate({ format: "formatted" });
+            
+		};
+
+		//method returns the day of the week (from 0 to 6) for the specified date.
+		function getDayName(){
+			var d = new Date();
+			var weekday = new Array(7);
+			weekday[0]=  "Sunday";
+			weekday[1] = "Monday";
+			weekday[2] = "Tuesday";
+			weekday[3] = "Wednesday";
+			weekday[4] = "Thursday";
+			weekday[5] = "Friday";
+			weekday[6] = "Saturday";
+			var gda = d.getDay();
+			var n = weekday[gda]; 
+		};
+
+		//returns the month (from 0 to 11) for the specified date, according to local time.
+		function getMonthName(){
+			var d = new Date();
+			var month = new Array(12);
+			month[0]=  "January";
+			month[1] = "February";
+			month[2] = "March";
+			month[3] = "April";
+			month[4] = "May";
+			month[5] = "June";
+			month[6] = "July";
+			month[7] = "August";
+			month[8] = "September";
+			month[9] = "October";
+			month[10] = "November";
+			month[11] = "December";
+			var gm = d.getMonth();
+			var m = month[gm];
+		};
+
+		function isFuture(){
+
+		};
+
+		function isToday(){
+
+		};
+		return {
+			//setDate: setDate,
+			getDate: getDate,
+			getDayName: getDayName,
+			getMonthName: getMonthName,
+			isFuture: isFuture,
+			isToday: isToday,
+		};
+		
+	}
+    //end life
+};//end myObj
