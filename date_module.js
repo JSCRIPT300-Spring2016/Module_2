2aa348867884e35457dd27ec18482eb9f8263d6f
lperry <laurellperry@gmail.com>
2016-04-21 18:34:15 -0700

    added date methods

--- date_module.js
+++ date_module.js
@@ -3,7 +3,7 @@
 // setDate is a method that allows the value of the date variable to be changed by user input
 // user enters a new date which is saved to variable
 
-var foo = ( function iife() {
+var myObj = ( function iife() {
   var myDate = new Date();
 
   function setDate() {
@@ -13,27 +13,68 @@ var foo = ( function iife() {
     }
   }
 
-  function getDate() {
-    myDate.getDate({ format: 'milliseconds' });
-    myDate.getDate({ format: 'formatted' });
+  function getDate( format ) {
+  	myDate.getDate({ format: "milliseconds" });
+/*
+  	  return myDate.getMilliseconds();
+    myDate.getDate({ format: "formatted" });
+
+      return myDate.getDayName + myDate.getMonthName = myDate.getFullYear;
+    myDate.getDate();
+  */
   }
 
   function getDayName() {
-    console.log(myDate.getDayName()); // "Monday"
+    var weekday = new Array(7);
+    weekday[0]=  'Sunday';
+    weekday[1] = 'Monday';
+    weekday[2] = 'Tuesday';
+    weekday[3] = 'Wednesday';
+    weekday[4] = 'Thursday';
+    weekday[5] = 'Friday';
+    weekday[6] = 'Saturday';
+
+    return weekday[myDate.getDay()];
   }
 
   function getMonthName() {
-    console.log(myDate.getMonthName()); // "April"
+    var month = new Array();
+    month[0] = 'January';
+    month[1] = 'February';
+    month[2] = 'March';
+    month[3] = 'April';
+    month[4] = 'May';
+    month[5] = 'June';
+    month[6] = 'July';
+    month[7] = 'August';
+    month[8] = 'September';
+    month[9] = 'October';
+    month[10] = 'November';
+    month[11] = 'December';
+
+    return month[myDate.getMonth()];
   }
 
   function isFuture() {
-    console.log(myDate.isFuture()); // false
+    if (myDate > Date.now){
+
+    	return true;
+    } else {
+
+    	return false;
+    }
   }
 
   function isToday() {
-    console.log(myDate.isToday()); // true
-  }
+    if (myDate.getDay === Date.now.getDay && mydate.getMonth ===
+   	  Date.now.getMonth && myDate.getFullYear === Date.now.getFullYear) {
 
+    	return true;
+    } else {
+
+    	return false;
+    }
+  }
 
   return {
     setDate: setDate,
@@ -45,4 +86,3 @@ var foo = ( function iife() {
   };
 })();
 
-foo.isFuture(); // 'baz'
