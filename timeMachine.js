0c8783172b8976dfdd35506231c6691a6e48134b
heidiflammable <heidiflammable@gmail.com>
2016-04-21 18:49:36 -0700

    adding assignment 2

---
+++ timeMachine.js
@@ -0,0 +1,54 @@
+'use strict';
+var timeMachine = (function iife() {
+  var date = new Date();
+  var month = new Array(12);
+  month[0] = 'January';
+  month[1] = 'February';
+  month[2] = 'March';
+  month[3] = 'April';
+  month[4] = 'May';
+  month[5] = 'June';
+  month[6] = 'July';
+  month[7] = 'August';
+  month[8] = 'September';
+  month[9] = 'October';
+  month[10] = 'November';
+  month[11] = 'December';
+
+  var weekday = new Array (7);
+  weekday [0] = 'Sunday';
+  weekday [1] = 'Monday';
+  weekday [2] = 'Tuesday';
+  weekday [3] = 'Wednesday';
+  weekday [4] = 'Thursday';
+  weekday [5] = 'Friday';
+  weekday [6] = 'Saturday';
+
+  /*will make sure input for var date is a date if not,
+    it will force it to become so
+  function setDate(dateInput) {
+    if (dateInput === Date) {
+      date = new Date(dateInput);
+    } else {
+     date = new Date();
+    }
+    return;
+  }*/
+
+  function getDayName() {
+    return weekday[ date.getDay() ];
+  }
+
+  function getMonthName() {
+    return month[ date.getMonth() ];
+  }
+
+  return {
+    //setDate: setDate,
+    getDayName: getDayName,
+    getMonthName: getMonthName
+  };
+})();
+
+timeMachine;
+
