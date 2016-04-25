772e8b668c9eeb74bcded2e1f7d403512b8cf007
Josh Smith <joshuap.smith@gmail.com>
2016-04-25 17:27:18 -0500

    Added lint file and fixed errors on assignment file

--- assignment_2_Script.js
+++ assignment_2_Script.js
@@ -1,81 +1,81 @@
 var dateCreation = (function iife() {
-    var currentModuleDate = null;
-    function instantiateDate() {
-        return new Date();
+  var currentModuleDate = null;
+  function instantiateDate() {
+    return new Date();
+  }
+  function setDate(date) {
+    if (date===undefined) {
+      currentModuleDate = instantiateDate();
+    }
+    else if (typeof date === 'number') {
+      currentModuleDate = new Date(date);
+    }
+    else if (date instanceof Date) {
+      currentModuleDate = new Date(date);
+    }
+  }
+  function getDate(requestedFormat) {
+    if (currentModuleDate === null) {
+      currentModuleDate = instantiateDate();
+    }
+    if (requestedFormat.format === 'formatted') {
+      return currentModuleDate.getMonth() + ' ' + currentModuleDate.getDate() +
+      ', ' + currentModuleDate.getFullYear();
+    }
+    else if (requestedFormat.format==='milliseconds' ||
+    requestedFormat===undefined) {
+      return currentModuleDate.getDate();
+    }
+  }
+  function getDayName() {
+    var day = ['Sunday','Monday', 'Tuesday', 'Wednesday',
+    'Thursday', 'Friday', 'Saturday'];
+    if (currentModuleDate === null) {
+      return day[instantiateDate().getDay()];
     }
 
-    function setDate(date) {
-        if (date===undefined) {
-            currentModuleDate = instantiateDate();
-        }
-        else if (typeof date === 'number') {
-            currentModuleDate = new Date(date);
-        }
-        else if (typeof date === 'object') {
-            currentModuleDate = date;
-        }
+    return day[currentModuleDate.getDay()];
+  }
+  function getMonthName() {
+    var month = ['January', 'February', 'March', 'April', 'May',
+    'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    if (currentModuleDate === null) {
+      return month[instantiateDate().getMonth()];
     }
 
-    function getDate(requestedFormat) {
-        if (currentModuleDate === null) {
-            currentModuleDate = instantiateDate();
-        }
-        if (requestedFormat.format === "formatted") {
-            return currentModuleDate.getMonth() + " " + currentModuleDate.getDate() + ", " + currentModuleDate.getFullYear();
-        }
-        else if (requestedFormat.format === "milliseconds" || requestedFormat === undefined) {
-            return currentModuleDate.getDate();
-        }
+    return month[currentModuleDate.getMonth()];
+  }
+  function isFuture() {
+    if (currentModuleDate === null) {
+      currentModuleDate = instantiateDate();
     }
-
-    function getDayName() {
-        var day = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        if (currentModuleDate === null) {
-            return day[instantiateDate().getDay()];
-        }
-
-        return day[currentModuleDate.getDay()];
+    if(currentModuleDate.getTime() > Date.now()) {
+      return true;
     }
-
-    function getMonthName() {
-        var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        if (currentModuleDate === null) {
-            return month[instantiateDate().getMonth()];
-        }
-        return month[currentModuleDate.getMonth()];
+    else {
+      return false;
     }
-
-    function isFuture() {
-        if (currentModuleDate === null) {
-            currentModuleDate = instantiateDate();
-        }
-        if(currentModuleDate.getTime() > Date.now()) {
-            return true;
-        }
-        else {
-            return false;
-        }
+  }
+  function isToday() {
+    if (currentModuleDate === null) {
+      currentModuleDate = instantiateDate();
     }
-
-    function isToday() {
-        if (currentModuleDate === null) {
-            currentModuleDate = instantiateDate();
-        }
-        var currentTime = new Date();
-        if( currentModuleDate.getFullYear() === currentTime.getFullYear() && currentModuleDate.getDay() === currentTime.getDay()) {
-            return true;
-        }
-        else {
-            return false;
-        }
+    var currentTime = new Date();
+    if( currentModuleDate.getFullYear() === currentTime.getFullYear() &&
+    currentModuleDate.getDay() === currentTime.getDay()) {
+      return true;
     }
+    else {
+      return false;
+    }
+  }
 
-    return {
-        setDate: setDate,
-        getDate: getDate,
-        getDayName: getDayName,
-        getMonthName: getMonthName,
-        isFuture: isFuture,
-        isToday: isToday
-    };
+  return {
+    setDate: setDate,
+    getDate: getDate,
+    getDayName: getDayName,
+    getMonthName: getMonthName,
+    isFuture: isFuture,
+    isToday: isToday
+  };
 })();
